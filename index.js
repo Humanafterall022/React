6a8c0bdda34b70af39813b4fbe3d18c6b5c28eb9
Miguel Tinta <mtinta@unsa.edu.pe>
2023-03-03 12:27:00 -0500

    ReactComponentes

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-ReactDOM.render(<h1>Hola</h1>, document.getElementById("root"))
 
-const h1 = document.createElement("h1")
-h1.textContent="gato"
-h1.className= "header"
-document.getElementById("root").append(h1)
+function Page(){
+    return(
+        <div>
+            <h1>Cosas que me gustan de React</h1>
+            <ol>
+                <li>Composable</li>
+                <li>Usa JS</li>
+                <li>DInero</li>
+            </ol>
+        </div>
+    )
+}
+
+
+function Header(){
+    return(
+        <nav>
+            <img src="./foto.png"></img>
+        </nav>
+    )
+}
+
+function Footer(){
+    return(
+        <footer>
+            <small>"© 20xx  development. All rights reserved."</small>
+        </footer>
+    )
+}
+ReactDOM.render(<div>
+                <Header/>
+                <Page/>
+                <Footer/>
+                </div>
+, document.getElementById("root"))
